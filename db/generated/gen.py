7e61b5b004b8ef7e9b897d982cc5f5cc146ab644
Jamie Wang <sjw59@duke.edu>
2022-10-13 20:10:08 -0400

    began editing gen.py

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -3,8 +3,18 @@ import csv
 from faker import Faker
 
 num_users = 100
-num_products = 2000
-num_purchases = 2500
+num_drinks = 500
+num_ingredients = 100
+num_components = 2000
+num_menus = 200
+num_menu_drinks = 2500
+num_ingredient_carts = num_users
+num_bar_carts = num_users
+num_ratings = 1000
+
+
+#num_products = 2000
+#num_purchases = 2500
 
 Faker.seed(0)
 fake = Faker()
@@ -30,8 +40,31 @@ def gen_users(num_users):
             lastname = name_components[-1]
             writer.writerow([uid, email, password, firstname, lastname])
         print(f'{num_users} generated')
+    return available_uids
+    
+def gen_drinks(num_drinks):
+    return available_dids
+    
+def gen_ingredients(num_ingredients):
+    return available_iids
+    
+def gen_components(num_components, available_dids, available_iids):
     return
 
+def gen_menus(num_menus, available_uids):
+    return available_menu_names
+    
+def gen_menu_drinks(num_menu_drinks, available_uids, available_menu_names, available_dids):
+    return
+    
+def gen_ingredient_cart(num_ingredient_carts, available_uids, available_iids):
+    return
+
+def gen_bar_cart(num_bar_carts, available_uids, available_dids):
+    return
+    
+def gen_ratings(num_ratings, available_uids, available_dids):
+    return
 
 # def gen_products(num_products):
 #     available_pids = []
@@ -66,6 +99,15 @@ def gen_users(num_users):
 #     return
 
 
-gen_users(num_users)
-available_pids = gen_products(num_products)
-gen_purchases(num_purchases, available_pids)
+available_uids = gen_users(num_users)
+available_dids = gen_drinks(num_drinks)
+available_iids = gen_ingredients(num_ingredients)
+available_menu_names = gen_menus(num_menus, available_uids, available_dids)
+gen_menu_drinks(num_menu_drinks, available_uids, available_menu_names, available_dids
+gen_ingredient_cart(available_uids, available_iids)
+gen_components(available_dids, available_iids)
+gen_bar_cart(available_uids, available_dids)
+gen_ratings(available_uids, available_dids)
+
+#available_pids = gen_products(num_products)
+#gen_purchases(num_purchases, available_pids)

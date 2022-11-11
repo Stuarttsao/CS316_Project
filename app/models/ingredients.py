29a111f0b98ef8d2eada45f29b42c4b499abff3e
Cheryl Li <crl45@duke.edu>
2022-11-10 19:32:11 -0500

    CL: entry page and css updates

--- app/models/ingredients.py
+++ app/models/ingredients.py
@@ -1,17 +1,20 @@
 from flask import current_app as app
 
 class Ingredients:
-    def __init__(self, iid, name):
-        self.iid = iid
+    def __init__(self, name, category, instructions, picture):
+        # drink characteristics
         self.name = name
-        
+        self.category = category
+        self.instructions = instructions
+        self.picture = picture
 
     @staticmethod
+    # search drinks by ingredients list
     def get_by_ingredient(name):
         rows = app.db.execute('''
-SELECT DISTINCT i.name, d.name 
-FROM Components c, Drinks d, Ingredients i
-WHERE i.name = :name and c.iid = i.iid and d.did = c.did
-''',
+            SELECT DISTINCT d.name, d.category, d.instructions, d.picture
+            FROM Components c, Drinks d, Ingredients i
+            WHERE i.name = :name and c.iid = i.iid and d.did = c.did
+            ''',
                               name=name)
         return [Ingredients(*row) for row in rows]
